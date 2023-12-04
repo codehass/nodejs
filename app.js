14bf8203d9f8845562c7abc9e67ca86a59e16b82
Hassan EL OUARDY <hassan.elouardy06@gmail.com>
2023-12-04 22:18:07 +0100

    handle post requests

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const fs = require('fs');
 const express = require('express');
 
 const app = express();
+app.use(express.json());
 
 // app.get('/', (req, res) => {
 //   res.status(200).send({ message: 'Hello from server side!', app: 'Natours' });
@@ -23,6 +24,25 @@ app.get('/api/v1/tours', (req, res) => {
       tours: tours,
     },
   });
+});
+
+app.post('/api/v1/tours', (req, res) => {
+  // console.log(req.body);
+  const newId = tours[tours.length - 1].id + 1;
+  const newTour = Object.assign({ id: newId }, req.body);
+  tours.push(newTour);
+  fs.writeFile(
+    `${__dirname}/dev-data/data/tours-simple.json`,
+    JSON.stringify(tours),
+    (err) => {
+      res.status(201).json({
+        status: 'success',
+        data: {
+          tour: newTour,
+        },
+      });
+    }
+  );
 });
 
 const port = 3000;
