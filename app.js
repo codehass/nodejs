476e5a2b20594fa1b8a51060f1f535388719508b
Hassan EL OUARDY <hassan.elouardy06@gmail.com>
2023-12-05 19:46:26 +0100

    handling patch request

--- app.js
+++ app.js
@@ -65,6 +65,21 @@ app.post('/api/v1/tours', (req, res) => {
   );
 });
 
+app.patch('/api/v1/tours/:id', (req, res) => {
+  if (req.params.id * 1 > tours.length)
+    return res.status(404).json({
+      status: 'fail',
+      message: 'Invalid Id',
+    });
+
+  res.status(200).json({
+    status: 'success',
+    data: {
+      tour: '<Updated tour...>',
+    },
+  });
+});
+
 const port = 3000;
 app.listen(port, () => {
   console.log(`App listen on port ${port}...`);
