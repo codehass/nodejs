1ae1640fd0e383fea12d745997373ce355c390bb
Hassan EL OUARDY <hassan.elouardy06@gmail.com>
2023-12-05 20:45:31 +0100

    create a middleware

--- app.js
+++ app.js
@@ -4,14 +4,26 @@ const express = require('express');
 const app = express();
 app.use(express.json());
 
+app.use((req, res, next) => {
+  console.log('Hello from the middleware 👋');
+  next();
+});
+
+app.use((req, res, next) => {
+  req.requestTime = new Date().toISOString();
+  next();
+});
+
 const tours = JSON.parse(
   fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`)
 );
 
 const getTours = (req, res) => {
+  console.log(req.requestTime);
   res.status(200).json({
     status: 'success',
     result: tours.length,
+    time: req.requestTime,
     data: {
       tours: tours,
     },
