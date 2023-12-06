759ff6b94cd379d0bb0435c97631d41b9cebd402
Hassan EL OUARDY <hassan.elouardy06@gmail.com>
2023-12-06 18:48:54 +0100

    fix linters

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -52,7 +52,7 @@ exports.getTour = (req, res) => {
 exports.createTour = (req, res) => {
   // console.log(req.body);
   const newId = tours[tours.length - 1].id + 1;
-  const newTour = Object.assign({ id: newId }, req.body);
+  const newTour = { id: newId, ...req.body };
   tours.push(newTour);
   fs.writeFile(
     `${__dirname}/dev-data/data/tours-simple.json`,
--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -5,19 +5,12 @@ const {
   createUser,
   getUser,
   updateUser,
-  deleteUser
-} = require('./../controllers/userController');
+  deleteUser,
+} = require('../controllers/userController');
 
 const router = express.Router();
 
-router
-  .route('/')
-  .get(getAllUsers)
-  .post(createUser);
-router
-  .route('/:id')
-  .get(getUser)
-  .patch(updateUser)
-  .delete(deleteUser);
+router.route('/').get(getAllUsers).post(createUser);
+router.route('/:id').get(getUser).patch(updateUser).delete(deleteUser);
 
 module.exports = router;
