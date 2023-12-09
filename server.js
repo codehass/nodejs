e27e2208564c5305542045cddaa72902f5be73df
Hassan EL OUARDY <hassan.elouardy06@gmail.com>
2023-12-09 19:09:57 +0100

    connecting db

--- server.js
+++ server.js
@@ -1,9 +1,20 @@
+const mongoose = require('mongoose');
 const dotenv = require('dotenv');
-
-dotenv.config({ path: './config.env' });
 const app = require('./app');
 
-// console.log(process.env);
+dotenv.config({ path: './config.env' });
+
+const DB = process.env.DATABASE.replace(
+  '<password>',
+  process.env.DATABASE_PASSWORD,
+);
+
+mongoose
+  .connect(DB, {
+    useUnifiedTopology: true,
+    useNewUrlParser: true,
+  })
+  .then(() => console.log('DB connection successful'));
 
 // 4) START SERVER
 const port = process.env.PORT || 3000;
