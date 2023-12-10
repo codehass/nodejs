b64e65fc8f8e0f40a56a0a6d50a0c0fb67a648d1
Hassan EL OUARDY <hassan.elouardy06@gmail.com>
2023-12-10 13:00:41 +0100

    Add filtering

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -1,8 +1,26 @@
+// const { query } = require('express');
 const Tour = require('../model/tourModel');
 
 exports.getAllTours = async (req, res) => {
   try {
-    const tours = await Tour.find();
+    // BUILD QUERY
+    const queryObj = { ...req.query };
+    const excludedFields = ['page', 'sort', 'limit', 'fields'];
+
+    excludedFields.forEach((el) => delete queryObj[el]);
+
+    const query = Tour.find(queryObj);
+
+    // const query = await Tour.find()
+    //   .where('duration')
+    //   .equals(5)
+    //   .where('difficulty')
+    //   .equals('easy');
+
+    // EXECUTE QUERY
+    const tours = await query;
+
+    // SEND RESPONSE
     res.status(200).json({
       status: 'success',
       result: tours.length,
