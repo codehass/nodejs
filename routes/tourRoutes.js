bc4620ede055299e8168487cd50d72eff41d7d07
Hassan EL OUARDY <hassan.elouardy06@gmail.com>
2023-12-11 21:27:00 +0100

    unwinding and projecting

--- routes/tourRoutes.js
+++ routes/tourRoutes.js
@@ -7,6 +7,7 @@ const {
   deleteTour,
   aliasTopTours,
   getToursStats,
+  getMonthlyPlan,
 } = require('../controllers/tourController');
 
 // 2) ROUTES HANDLERS
@@ -16,6 +17,7 @@ const router = express.Router();
 
 router.route('/top-5-cheap').get(aliasTopTours, getAllTours);
 router.route('/tour-stats').get(getToursStats, getAllTours);
+router.route('/monthly-plan/:year').get(getMonthlyPlan, getAllTours);
 router.route('/').get(getAllTours).post(createTour);
 router.route('/:id').get(getTour).patch(updateTour).delete(deleteTour);
 
