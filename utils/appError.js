c8e1aa316250182a58bbd89230419c6f5e9aab40
Hassan EL OUARDY <hassan.elouardy06@gmail.com>
2023-12-14 21:48:53 +0100

    error handling and refactoring

---
+++ utils/appError.js
@@ -0,0 +1,14 @@
+class AppError extends Error {
+  constructor(message, statusCode) {
+    super(message);
+
+    this.statusCode = statusCode;
+    this.status = `${statusCode}`.startsWith('4') ? 'fail' : 'error';
+    this.isOperational = true;
+
+    //where the error is happen.
+    Error.captureStackTrace(this, this.constructor);
+  }
+}
+
+module.export = AppError;
