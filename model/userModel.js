4184c14ce5d47206d4e7ed49703eaffc897db49f
Hassan EL OUARDY <hassan.elouardy06@gmail.com>
2023-12-16 19:24:02 +0100

    Modeling users

---
+++ model/userModel.js
@@ -0,0 +1,37 @@
+const mongoose = require('mongoose');
+const validator = require('validator');
+
+// name, email, photo, password, passwordConfirm
+const userSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: [true, 'please tell us your name!'],
+    },
+    email: {
+      type: String,
+      required: [true, 'Please proved your email!'],
+      unique: true,
+      lowercase: true,
+      validate: [validator.isEmail, 'Please proved a valid email!'],
+    },
+    photo: String,
+    password: {
+      type: String,
+      required: [true, 'Please proved a password!'],
+      minlength: 8,
+    },
+    passwordConfirm: {
+      type: String,
+      required: [true, 'Please confirm your password!'],
+    },
+  },
+  {
+    toJSON: { virtuals: true },
+    toObject: { virtuals: true },
+  },
+);
+
+const User = mongoose.model('Tour', userSchema);
+
+module.exports = User;
