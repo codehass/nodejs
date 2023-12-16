b68d1a32c1bd3b860f343973ff7f6de57bb32440
Hassan EL OUARDY <hassan.elouardy06@gmail.com>
2023-12-16 18:09:15 +0100

    unhandled rejection

--- server.js
+++ server.js
@@ -18,6 +18,14 @@ mongoose
 
 // 4) START SERVER
 const port = process.env.PORT || 3000;
-app.listen(port, () => {
+const server = app.listen(port, () => {
   console.log(`App listen on port ${port}...`);
+});
+
+process.on('unhandledRejection', (err) => {
+  console.log(err.name, err.message);
+  console.log('UNHANDLED REJECTION! 💥 Shuting down...');
+  server.close(() => {
+    process.exit(1);
+  });
 });
