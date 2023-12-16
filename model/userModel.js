4fa093672c1042f4c6270b2e1fc58ee7848e2a9c
Hassan EL OUARDY <hassan.elouardy06@gmail.com>
2023-12-16 21:42:03 +0100

    managing passwords

--- model/userModel.js
+++ model/userModel.js
@@ -1,5 +1,6 @@
 const mongoose = require('mongoose');
 const validator = require('validator');
+const bcrypt = require('bcryptjs');
 
 // name, email, photo, password, passwordConfirm
 const userSchema = new mongoose.Schema({
@@ -23,7 +24,26 @@ const userSchema = new mongoose.Schema({
   passwordConfirm: {
     type: String,
     required: [true, 'Please confirm your password!'],
+    validate: {
+      //This only works on CREATE and SAVE
+      validator: function (el) {
+        return el === this.password;
+      },
+      message: 'Password are not the same!',
+    },
   },
+});
+
+userSchema.pre('save', async function (next) {
+  // only run this function if the password was actually modified
+  if (!this.isModified('password')) return next();
+
+  // Hash the password with the cost of 12
+  this.password = await bcrypt.hash(this.password, 12);
+
+  // Delete the password confirm field
+  this.passwordConfirm = undefined;
+  next();
 });
 
 const User = mongoose.model('User', userSchema);
