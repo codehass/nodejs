27ab24ca67be97613af3fc1fa6234964ef995f11
Hassan EL OUARDY <hassan.elouardy06@gmail.com>
2023-12-17 19:25:56 +0100

    login in users

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -7,11 +7,12 @@ const {
   updateUser,
   deleteUser,
 } = require('../controllers/userController');
-const authController = require('../controllers/authController');
+const { signup, login } = require('../controllers/authController');
 
 const router = express.Router();
 
-router.post('/signup', authController.signup);
+router.post('/signup', signup);
+router.post('/login', login);
 
 router.route('/').get(getAllUsers).post(createUser);
 router.route('/:id').get(getUser).patch(updateUser).delete(deleteUser);
