144cf87ff31afa24e8fddc69b03867005a9e44f0
Hassan EL OUARDY <hassan.elouardy06@gmail.com>
2023-12-27 17:31:09 +0100

    deleting current user

--- controllers/userController.js
+++ controllers/userController.js
@@ -23,6 +23,13 @@ exports.getAllUsers = catchAsync(async (req, res, next) => {
   });
 });
 
+exports.createUser = (req, res) => {
+  res.status(500).json({
+    status: 'error',
+    message: 'This route is not yet defined!',
+  });
+};
+
 exports.updateMe = catchAsync(async (req, res, next) => {
   // 1) Create error if user POSTs password data
   if (req.body.password || req.body.passwordConfirm) {
@@ -51,12 +58,14 @@ exports.updateMe = catchAsync(async (req, res, next) => {
   });
 });
 
-exports.createUser = (req, res) => {
-  res.status(500).json({
-    status: 'error',
-    message: 'This route is not yet defined!',
+exports.deleteMe = catchAsync(async (req, res, next) => {
+  await User.findByIdAndUpdate(req.user.id, { active: false });
+
+  res.status(204).json({
+    status: 'success',
+    data: null,
   });
-};
+});
 
 exports.getUser = (req, res) => {
   res.status(500).json({
--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -7,6 +7,7 @@ const {
   updateUser,
   deleteUser,
   updateMe,
+  deleteMe,
 } = require('../controllers/userController');
 const {
   signup,
@@ -27,6 +28,8 @@ router.patch('/resetPassword/:token', resetPassword);
 
 router.patch('/updateMyPassword', protect, updatePassword);
 router.patch('/updateMe', protect, updateMe);
+router.delete('/deleteMe', protect, deleteMe);
+
 router.route('/').get(getAllUsers).post(createUser);
 router.route('/:id').get(getUser).patch(updateUser).delete(deleteUser);
 
