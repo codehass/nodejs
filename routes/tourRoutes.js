b41f62e5877ddc0ff0e29b7f0986f188665a4868
codehass <hassan.elouardy06@gmail.com>
2024-02-04 21:19:36 +0100

    Nested routes with express

--- routes/tourRoutes.js
+++ routes/tourRoutes.js
@@ -10,12 +10,17 @@ const {
   getMonthlyPlan,
 } = require('../controllers/tourController');
 const { protect, restrictTo } = require('../controllers/authController');
-const { createReview } = require('../controllers/reviewController');
+const reviewRouter = require('./reviewRoutes');
 
 // 2) ROUTES HANDLERS
 const router = express.Router();
 
 // router.param('id', checkId);
+
+// POST tour/24516987/reviews
+// GET tour/24516987/reviews
+
+router.use('/:tourId/reviews', reviewRouter);
 
 router.route('/top-5-cheap').get(aliasTopTours, getAllTours);
 router.route('/tour-stats').get(getToursStats, getAllTours);
@@ -26,13 +31,5 @@ router
   .get(getTour)
   .patch(updateTour)
   .delete(protect, restrictTo('admin', 'lead-guide'), deleteTour);
-
-// POST tour/24516987/reviews
-// GET tour/24516987/reviews
-// GET tour/24516987/reviews/784210369
-
-router
-  .route('/:tourId/reviews')
-  .post(protect, restrictTo('user'), createReview);
 
 module.exports = router;
