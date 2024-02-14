8a9c3311bf8623b2fb799d5fcd48e45bc051d9db
codehass <hassan.elouardy06@gmail.com>
2024-02-14 21:23:29 +0100

    add /me en point

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -8,6 +8,7 @@ const {
   deleteUser,
   updateMe,
   deleteMe,
+  getMe,
 } = require('../controllers/userController');
 
 const {
@@ -28,6 +29,9 @@ router.post('/forgetPassword', forgetPassword);
 router.patch('/resetPassword/:token', resetPassword);
 
 router.patch('/updateMyPassword', protect, updatePassword);
+
+router.get('/me', protect, getMe, getUser);
+
 router.patch('/updateMe', protect, updateMe);
 router.delete('/deleteMe', protect, deleteMe);
 
