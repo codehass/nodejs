a88cbc4a32c0c92ebe2fc1b738f24b30ce5de048
codehass <hassan.elouardy06@gmail.com>
2024-02-14 22:50:15 +0100

    adding missing authentication and authorization

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -18,22 +18,25 @@ const {
   forgetPassword,
   resetPassword,
   updatePassword,
+  restrictTo,
 } = require('../controllers/authController');
 
 const router = express.Router();
 
 router.post('/signup', signup);
 router.post('/login', login);
-
 router.post('/forgetPassword', forgetPassword);
 router.patch('/resetPassword/:token', resetPassword);
 
-router.patch('/updateMyPassword', protect, updatePassword);
+//Protect all routes after this middleware
+router.use(protect);
 
-router.get('/me', protect, getMe, getUser);
+router.patch('/updateMyPassword', updatePassword);
+router.get('/me', getMe, getUser);
+router.patch('/updateMe', updateMe);
+router.delete('/deleteMe', deleteMe);
 
-router.patch('/updateMe', protect, updateMe);
-router.delete('/deleteMe', protect, deleteMe);
+router.use(restrictTo('admin'));
 
 router.route('/').get(getAllUsers).post(createUser);
 router.route('/:id').get(getUser).patch(updateUser).delete(deleteUser);
