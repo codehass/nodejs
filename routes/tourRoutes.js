c1ee1b48855a75a1ec9a351652b9440730bf1a2d
codehass <hassan.elouardy06@gmail.com>
2024-02-22 13:54:55 +0100

    Geospatial queries finding tours within radius

--- routes/tourRoutes.js
+++ routes/tourRoutes.js
@@ -8,6 +8,7 @@ const {
   aliasTopTours,
   getToursStats,
   getMonthlyPlan,
+  getToursWithin,
 } = require('../controllers/tourController');
 const { protect, restrictTo } = require('../controllers/authController');
 const reviewRouter = require('./reviewRoutes');
@@ -21,6 +22,12 @@ const router = express.Router();
 // GET tour/24516987/reviews
 
 router.use('/:tourId/reviews', reviewRouter);
+
+router
+  .route('/tours-within/:distance/center/:latlng/unit/:unit')
+  .get(getToursWithin);
+// /tours-within?distance=200&center=-40,45&unit=mi
+// /tour-within/200/center/-40,45/unit/mi
 
 router.route('/top-5-cheap').get(aliasTopTours, getAllTours);
 router.route('/tour-stats').get(getToursStats, getAllTours);
